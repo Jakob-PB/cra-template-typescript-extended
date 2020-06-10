23fd4f91045c1da46c971365aac331bbf64ffa86
Jakob Bjerkness <jakobpb@gmail.com>
2020-06-10 16:13:09 -0500

    build: Change commitlint rules to force subject to start uppercase

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,12 @@
+// Rules from "@commitlint/config-conventional":
+//     source: node_modules/@commitlint/config-conventional/index.js
+
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "header-max-length": [2, "always", 72],
     "body-max-line-length": [2, "always", 72],
     "footer-max-line-length": [2, "always", 72],
+    "subject-case": [2, "never", ["lower-case"]],
   },
 };
