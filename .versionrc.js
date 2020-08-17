fb8e4843f97155ddcf575bdc495ca777bf6a310d
Jakob Bjerkness <jakobpb@gmail.com>
2020-08-16 22:14:49 -0500

    refactor: Add standard-version config file

---
+++ .versionrc.js
@@ -0,0 +1,16 @@
+// Configuration for standard-version
+
+module.exports = {
+  types: [
+    {type: "feat", section: "Features"},
+    {type: "fix", section: "Bug Fixes"},
+    {type: "chore", section: "Chores"},
+    {type: "docs", hidden: true},
+    {type: "style", hidden: true},
+    {type: "refactor", hidden: true},
+    {type: "perf", hidden: true},
+    {type: "test", hidden: true},
+    {type: "build", hidden: true},
+    {type: "ci", hidden: true},
+  ]
+};
